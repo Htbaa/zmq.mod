c8106b8811bbe95ae3efa1d469bc6253469ff0fe
Htbaa <c.kras@pcc-online.net>
2012-02-06 13:02:32 +0100

    Use proviced zmq headers

--- glue.c
+++ glue.c
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-#include <zmq.h>
+#include "zeromq/include/zmq.h"
 
 zmq_msg_t * bmx_zmq_message_t() {
 	zmq_msg_t *msg;
